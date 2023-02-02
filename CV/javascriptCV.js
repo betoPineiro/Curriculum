e5089b86e7ee9b6010bb38fb3d2f82e2789cfa59
Daniel Piñeiro <pineiro.ad@gmail.com>
2023-02-01 22:24:27 -0300

    cv casi completo

--- CV/javascriptCV.js
+++ CV/javascriptCV.js
@@ -1,19 +1,19 @@
-alert("tocar si anda")
+/*
 function efectoIdiomas()
 {
     var misIdiomas = document.getElementById("misIdiomas");
     var distancia_misIdiomas = window.innerHeight - misIdiomas.getBoundingClientRect().top;
     if (distancia_misIdiomas >= 300)
     {
-        let skills = document.getElementsByClassName("progreso");
-        skills(0).classList.add("ingles");
-        /*habilidades(1).classList.add("Francés");
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades(0).classList.add("ingles");
+        habilidades(1).classList.add("Francés");
         habilidades(2).classList.add("Portugués");
         habilidades(3).classList.add("Chino");
         habilidades(4).classList.add("ETAP");
         habilidades(5).classList.add("autocad");
         habilidades(6).classList.add("SolidWorks");
-        habilidades(7).classList.add("GeoFryPlots");*/
+        habilidades(7).classList.add("GeoFryPlots");
     }
 }
 
@@ -22,13 +22,7 @@ window.onscroll = function()
     efectoIdiomas();
 }
 
-
-/*41.14
-skills
-progreso
-javascript
-49.56
-
-1.47.00
-.skills .skill .javascript
+    ESTA FUNCION DEBERIA HACER QUE LAS BARRAS DE IDIOMAS Y SOFTWARE 
+    AUMENTEN CUANDO HACEMOS SCROLL PERO NO ENCUENTRO EL ERROR QUE
+    HACE QUE NO FUNCIONE.
 */
